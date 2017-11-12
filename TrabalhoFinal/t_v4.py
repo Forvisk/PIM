40e855f5d0f35d8723e4c089d4bf63e29bd6b31e
Forvisk <adrianojr11@gmail.com>
2017-11-12 19:57:22 -0200

    borda parece ok

--- TrabalhoFinal/t_v4.py
+++ TrabalhoFinal/t_v4.py
@@ -144,8 +144,8 @@ def catchBorda( pat, inimg, ext, amostra):
 						borda[i][j] = 255
 
 	if ok:
-		output = input + "_getBorda_v3" + ".png"
-		scpm.imsave(pathtemp + output, borda)
+		output = input + "_getBorda_v4"
+		scpm.imsave(pathtemp + output + ".png", borda)
 		return output
 	if not ok:
 		return '-1'
@@ -159,9 +159,14 @@ def distanciaEucl( p1, p2):
 	#print dist
 	return dist
 
-def detectaBorda( pat, input):
-	print "Borda : " + pat + input
-	inimg = scpm.imread( pat + input)
+def pontoMedio( p1, p2):
+	x = ( p1[0] + p2[0]) / 2
+	y = ( p1[1] + p2[1]) / 2
+	return [x,y]
+
+def detectaBorda( pat, input, ext):
+	print "Borda : " + pat + input + ext
+	inimg = scpm.imread( pat + input + ext)
 	size = inimg.shape
 
 	M = size[0]
@@ -173,12 +178,14 @@ def detectaBorda( pat, input):
 	ExtrW = 0
 	ExtrE = N-1
 
-	borda = np.zeros( [M, N, 3] )
-	#borda = np.zeros( [M, N] )
+	#borda = np.zeros( [M, N, 3] )
+	borda = np.zeros( [M, N] )
+	'''
 	for i in range(0, M):
 		for j in range(0,N):
 			if inimg[i][j] == 255:
 				borda[i][j] = [100,100,100]
+	'''
 
 	while( ExtrW < MeioN and inimg[MeioM][ExtrW] == 0):
 		ExtrW += 1
@@ -202,17 +209,16 @@ def detectaBorda( pat, input):
 
 	if ok:
 
-
 		change = True
 		it = 10
 		turn = True
 		while( change and it > 0):
 
-			print [ ExtrN, ExtrS, ExtrW, ExtrE]
-
+			#print [ ExtrN, ExtrS, ExtrW, ExtrE]
+			'''
 			for i in range( 0, abs( ExtrS - ExtrN)):
 				borda[ExtrN + i][MeioN] = [255,255,255]
-
+			
 			for i in range( 0, abs( ExtrW - ExtrE)):
 				borda[MeioM][ExtrW+i] = [255,255,255]
 
@@ -221,22 +227,23 @@ def detectaBorda( pat, input):
 				borda[MeioM][ExtrE-i] = [0,255,0]	#Verde
 				borda[ExtrN+i][MeioN] = [0,0,255]	#Azul
 				borda[ExtrS-i][MeioN] = [255,0,255]	#Magenta
-
+			'''
 
 			#	r**2 = x**2 + y**2	(circunferencia)
 			r1 = abs( ExtrS - ExtrN) / 2
 			r2 = abs( ExtrW - ExtrE) / 2
 			c1 = [ExtrN + r1, MeioN]
 			c2 = [MeioM, ExtrW + r2]
-			print ( r1, r2)
-			print (c1, c2)
+			#print ( r1, r2)
+			#print (c1, c2)
 
+			'''
 			for i in range(-2, 3):
 				borda[c1[0] +i][c1[1] +i][0] = 255
 				borda[c1[0] -i][c1[1] +i][0] = 255
 				borda[c2[0] +i][c2[1] +i][1] = 255
 				borda[c2[0] -i][c2[1] +i][1] = 255
-
+			'''
 
 			if turn:
 				turn = False
@@ -274,10 +281,50 @@ def detectaBorda( pat, input):
 
 			it -= 1
 
+		r1 = abs( ExtrS - ExtrN) / 2
+		r2 = abs( ExtrW - ExtrE) / 2
+		c1 = [ExtrN + r1, MeioN]
+		c2 = [MeioM, ExtrW + r2]
+
+		centro 	= pontoMedio(c1, c2)
+
+		PNorte 	= [ centro[0] - r1, centro[1]]
+		PSul 	= [ centro[0] + r1, centro[1]]
+		PLeste	= [ centro[0], centro[1]+r2]
+		POeste 	= [ centro[0], centro[1]-r2]
+		
+		#print ( "Sul", PSul, "Norte", PNorte)
+		#print ( "Leste", PLeste, "Oeste", POeste)
+		#print centro
+
+		'''
+		for i in range(0,5):
+			borda[ PNorte[0]+i][ PNorte[1]] = [255,255,0]
+			borda[ PSul[0]-i][ PSul[1]] 	= [255,255,0]
+			borda[ PLeste[0]][ PLeste[1]-i] = [255,255,0]
+			borda[ POeste[0]][ POeste[1]+i] = [255,255,0]
+		'''
+		rangeBorda = 10*min(size[0], size[1]) / 700
+		#print rangeBorda
+
+		for i in range( 0, M):
+			for j in range( 0, N):
+				dist = distanciaEucl( centro, [i,j])
+				if ( dist > r1-rangeBorda and dist < r1+rangeBorda) or ( dist > r2-rangeBorda and dist < r2+rangeBorda):
+					#borda[i,j] = [255,255,255]
+					borda[i,j] = 255
+		'''
+		for i in range(0,rangeBorda):
+			borda[ PNorte[0]+i][ PNorte[1]] = [255,255,0]
+			borda[ PSul[0]-i][ PSul[1]] 	= [255,255,0]
+			borda[ PLeste[0]][ PLeste[1]-i] = [255,255,0]
+			borda[ POeste[0]][ POeste[1]+i] = [255,255,0]
+		'''
+
 
 
 	if ok:
-		output = input + "_borda_v3" + ".png"
+		output = input + "_borda_v4" + ".png"
 		scpm.imsave(pathtemp + output, borda)
 		return output
 	if not ok:
@@ -295,12 +342,34 @@ def roda( input, ext):
 		return "ERRO"
 	#else: return borda
 	print borda
-	borda = detectaBorda( pathtemp, borda)
+	ext2 = ".png"
+	borda = detectaBorda( pathtemp, borda, ext2)
 	if borda == '-1':
 		return "ERRO"
-	else: return borda
+	#else: return borda
+
+	original = scpm.imread(path+input+ext)
+	alt = scpm.imread(pathtemp+borda)
+	size = alt.shape
+	for i in range(size[0]):
+		for j in range(size[1]):
+			#if alt[i][j][0] != 0 and alt[i][j][1] != 0 and alt[i][j][2] != 0 :
+			if alt[i][j] != 0 :
+				original[i][j] = [alt[i][j],alt[i][j],alt[i][j]]
+	scpm.imsave(pathtemp+input+"_resp_v4"+".png", original)
+	return input+"_resp_v4"+".png"
 
 
+
+input = "placa_mancha"
+ext = ".jpg"
+print roda( input, ext)
+
+# placa_fungos.jpg possiu marcas de agua o que impossibilita a  utilizacao
+
+input = "placa_fungos"
+ext = ".jpg"
+print roda( input, ext)
 
 input = "placa_paint"
 ext = ".png"
@@ -310,21 +379,19 @@ input = "placa_listra"
 ext = ".jpg"
 print roda( input, ext)
 
-input = "placa_mancha"
+#placa_pontos2.jpg nao funciona
+
+input = "placa_pontos2"
 ext = ".jpg"
 print roda( input, ext)
 
-# placa_fungos.jpg possiu marcas de agua o que impossibilita a  utilizacao
-'''
-input = "placa_fungos"
+#placa_pontos.jpg nao funciona
+
+input = "placa_pontos"
 ext = ".jpg"
 print roda( input, ext)
-'''
 
-#input = "placa_pontos"
-#ext = ".jpg"
-#print roda( input, ext)
 
-#input = "placa_paint_2"
-#ext = ".png"
-#print roda( input, ext)
+input = "placa_paint_2"
+ext = ".png"
+print roda( input, ext)
